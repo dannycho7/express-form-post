0b77f9840fb768666904dfeaf555aebbb23064f5
dannycho7 <dannycho7@gmail.com>
2017-05-24 02:58:59 -0700

    diskstorage and fileupload updates based on params

--- src/lib/diskstorage.js
+++ src/lib/diskstorage.js
@@ -1,11 +1,35 @@
 const concat = require('concat-stream');
 const fs = require('fs');
+const path = require('path');
+const mkdirp = require('mkdirp');
 
-module.exports = (save_path) => {
+module.exports = (options, filename, mimetype, req, next) => {
 	return concat((data) => {
-		let writeStream = fs.createWriteStream(save_path);
-		writeStream.write(data, () => {
-			console.log("Successfully saved file to", save_path);
+		fs.stat(options.directory, (err, stats) => {
+			if (err) {
+				mkdirp(options.directory, (err) => {
+					if (err) {
+						console.log(err);
+					} else {
+						console.log("Created directory", options.directory);
+						cb(path.join(options.directory, filename), data);
+						next();
+					}
+				})
+			} else {
+				cb(path.join(options.directory, filename), data);
+				next();
+			}
 		});
+		
+	});
+}
+
+let cb = (save_path, data) => {
+	let writeStream = fs.createWriteStream(save_path);
+	writeStream.write(data, () => {
+		req.file = {
+			path: save_path
+		};
 	});
 }
