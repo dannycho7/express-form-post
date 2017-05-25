810d5ebb61a9ef9642499f902c7958de354be3c4
dannycho7 <dannycho7@gmail.com>
2017-05-25 10:28:12 -0700

    preparing field callback handler

--- test/app.js
+++ test/app.js
@@ -3,9 +3,8 @@ const path = require("path");
 require("dotenv").config({ path: path.join(__dirname, ".env") });
 const formPost = require("../index"); // npm module
 const formPostHandler = new formPost({
-	method: "disk-storage",
-	directory: path.join(__dirname, "tmp"),
-	maxfileSize: 1000000
+	storage: "disk-storage", // Optional field; defaults to "disk-storage"
+	directory: path.join(__dirname, 'tmp')
 });
 
 module.exports = (app) => {
