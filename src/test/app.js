c326c69f4bab2802de2c3dae5427dccdeec16644
dannycho7 <dannycho7@gmail.com>
2017-05-24 20:17:27 -0700

    new test implementation

--- src/test/app.js
+++ src/test/app.js
@@ -1,18 +1,19 @@
-const formPost = require('../index'); // npm module
 const express = require('express');
 const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '.env') });
+const formPost = require('../index'); // npm module
+const formPostHandler = new formPost({
+	method: 'disk-storage',
+	directory: path.join(__dirname, 'tmp'),
+	maxfileSize: 1000000
+});
 
 module.exports = (app) => {
 
 	app.use(express.static(path.join(__dirname, "static")));
 	app.set('view engine', 'ejs');
 	app.set('views', path.join(__dirname, "views"));
-	app.use(formPost({
-		method: 'disk-storage',
-		directory: path.join(__dirname, 'tmp'),
-		maxfileSize: 1000000
-	}));
+	app.use(formPostHandler.default());
 
 	/* 
 	app.use(formPost({
