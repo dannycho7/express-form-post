2a91018d9c6c2e08d9c8ff78f4d408f77452a6e7
dannycho7 <dannycho7@gmail.com>
2017-06-17 01:34:29 -0700

    err handling  through user param err handler

--- index.js
+++ index.js
@@ -4,25 +4,29 @@ const Busboy = require("busboy");
 const ExpressFormPost = function(user_options = {}) {
 	if(!(this instanceof ExpressFormPost)) return new ExpressFormPost(user_options);
 
-	this.options = {
-		store: user_options.method || "disk",
-		directory: user_options.directory || "tmp",
-		filename: user_options.filename,
-		validate: {
-			mimetype: user_options.mimetype || "",
-			fileSize: user_options.maxfileSize || ""
-		},
-		keys: user_options.keys || ""
-	};
-
-	// Available storage methods
-	if(!["disk", "s3"].includes(this.options.method)) {
-		this.options.method = "disk";
+	// validate
+	if(user_options.validate && typeof user_options.validate != "function") {
+		throw new Error("option 'validate' must be a function.");
 	}
 
-	if(typeof this.options.filename == "function") {
-		let customFileMethod = this.options.filename;
-		this.options.filename = function(filename, fieldname, mimetype) {
+	// max file size
+	if(user_options.maxfileSize && typeof user_options.maxfileSize != "number") {
+		throw new Error("option 'maxfileSize' must be a number.");
+
+		if(!user_options.maxfileSize.isInteger()) {
+			throw new Error("option 'maxfileSize' must be an integer (Measured in bytes).");
+		}
+	}
+
+	// Available storage methods
+	if(!["disk", "s3"].includes(user_options.store)) {
+		user_options.store = "disk";
+	}
+
+	// filename options setup
+	if(typeof user_options.filename == "function") {
+		let customFileMethod = user_options.filename;
+		user_options.filename = function(filename, fieldname, mimetype) {
 			let customName = customFileMethod(filename, fieldname, mimetype);
 			if(customName == undefined || customName == "") {
 				return filename; // returning the filename that is being uploaded
@@ -31,39 +35,55 @@ const ExpressFormPost = function(user_options = {}) {
 		}
 		
 	} else {
-		switch(this.options.filename) {
+		switch(user_options.filename) {
 			case undefined:
 			case "": 
-				this.options.filename = function(filename, fieldname, mimetype) {
+				user_options.filename = function(filename, fieldname, mimetype) {
 					return filename;
 				}
 				break;
 			default:
-				let user_input = this.options.filename; // Closures are awesome
-				this.options.filename = function(filename, fieldname, mimetype) {
+				let user_input = user_options.filename; // Closures are awesome
+				user_options.filename = function(filename, fieldname, mimetype) {
 					return user_input;
 				}
 		}
 	}
+
+	this.options = {
+		store: user_options.store,
+		directory: user_options.directory || "tmp",
+		filename: user_options.filename,
+		maxfileSize: user_options.maxfileSize,
+		validate: user_options.validate,
+		keys: user_options.keys
+	};
+
 };
 
 const storeInMemory = function(busboy, req, next) {
 	busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
-		if (this.options.validate.mimetype != "" && this.options.validate.mimetype != mimetype) {
+
+		if (this.options.validate(fieldname, filename, mimetype) == false) {
 			return;
 		}
 
 		let save_filename = this.options.filename(filename, fieldname, mimetype);
 
 		let uploadInfo = {
-			options: this.options,
+			directory: this.options.directory,
 			filename: save_filename,
 			mimetype: mimetype,
 			fieldname: fieldname,
 			file: file,
-			encoding: encoding
+			encoding: encoding,
+			keys: this.options.keys
 		};
-		let file_contents = require(path.join(__dirname, "lib", this.options.method))(uploadInfo, req, next);
+
+		// init concat-stream
+		const storeMethod = require(path.join(__dirname, "lib", this.options.store));
+		const file_contents = storeMethod(uploadInfo, req, next, this.handleError);
+
 		file.on("data", (data) => {
 			if (!req.files[fieldname]) {
 				req.files[fieldname] = {}; 
@@ -71,7 +91,7 @@ const storeInMemory = function(busboy, req, next) {
 			file_contents.write(data);
 		});
 		file.on("limit", () => {
-			console.error("File limit reached");
+			this.handleError(new Error("File limit reached on file"));
 		});
 		file.on("end", () => {
 			if (!file.truncated && req.files[fieldname]) {
@@ -104,7 +124,7 @@ const fileHandler = function(req, res, next) {
 		let busboy = new Busboy({ 
 			headers: req.headers,
 			limits: {
-				fileSize: this.options.validate.fileSize
+				fileSize: this.options.maxfileSize
 			}
 		});
 		storeInMemory.bind(this)(busboy, req, next);
@@ -114,8 +134,8 @@ const fileHandler = function(req, res, next) {
 	}
 };
 
-
-ExpressFormPost.prototype.middleware = function() {
+ExpressFormPost.prototype.middleware = function(handleError = undefined) {
+	typeof handleError == "function" ? this.handleError = handleError : this.handleError = (err) => {};
 	return fileHandler.bind(this);
 };
 
