5fc8fb3aff3fc5ee57ad023577c69c45b4d36345
dannycho7 <dannycho7@gmail.com>
2017-06-16 18:51:29 -0700

    custom filename function input and validation update

--- index.js
+++ index.js
@@ -2,18 +2,98 @@ const path = require("path");
 const Busboy = require("busboy");
 
 const ExpressFormPost = function(user_options = {}) {
+	if(!(this instanceof ExpressFormPost)) return new ExpressFormPost(user_options);
+
 	this.options = {
-		method: user_options.method || "disk-storage",
+		store: user_options.method || "disk",
 		directory: user_options.directory || "tmp",
-		filename: user_options.filename || "",
-		mimetype: user_options.mimetype || "",
-		limits: {
+		filename: user_options.filename,
+		validate: {
+			mimetype: user_options.mimetype || "",
 			fileSize: user_options.maxfileSize || ""
 		},
 		keys: user_options.keys || ""
 	};
+
+	// Available storage methods
+	if(!["disk", "s3"].includes(this.options.method)) {
+		this.options.method = "disk";
+	}
+
+	if(typeof this.options.filename == "function") {
+		let customFileMethod = this.options.filename;
+		this.options.filename = function(filename, fieldname, mimetype) {
+			let customName = customFileMethod(filename, fieldname, mimetype);
+			if(customName == undefined || customName == "") {
+				return filename; // returning the filename that is being uploaded
+			} 
+			return customName;
+		}
+		
+	} else {
+		switch(this.options.filename) {
+			case undefined:
+			case "": 
+				this.options.filename = function(filename, fieldname, mimetype) {
+					return filename;
+				}
+				break;
+			default:
+				let user_input = this.options.filename; // Closures are awesome
+				this.options.filename = function(filename, fieldname, mimetype) {
+					return user_input;
+				}
+		}
+	}
 };
 
+const storeInMemory = function(busboy, req, next) {
+	busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
+		if (this.options.validate.mimetype != "" && this.options.validate.mimetype != mimetype) {
+			return;
+		}
+
+		let save_filename = this.options.filename(filename, fieldname, mimetype);
+
+		let uploadInfo = {
+			options: this.options,
+			filename: save_filename,
+			mimetype: mimetype,
+			fieldname: fieldname,
+			file: file,
+			encoding: encoding
+		};
+		let file_contents = require(path.join(__dirname, "lib", this.options.method))(uploadInfo, req, next);
+		file.on("data", (data) => {
+			if (!req.files[fieldname]) {
+				req.files[fieldname] = {}; 
+			}
+			file_contents.write(data);
+		});
+		file.on("limit", () => {
+			console.error("File limit reached");
+		});
+		file.on("end", () => {
+			if (!file.truncated && req.files[fieldname]) {
+				req._files++; // increment count of files being uploaded to store
+				file_contents.end();
+			}
+			req._body = true;
+		});
+	});
+
+	busboy.on("field", (fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) => {
+		req.body[fieldname] = val;
+	});
+	busboy.on("finish", () => {
+		if(req._files == 0) {
+			// no file was uploaded
+			return next();
+		}
+	});
+};
+
+// Default
 const fileHandler = function(req, res, next) {
 	if(req.method == "POST") {
 		if(req._body) {
@@ -23,7 +103,9 @@ const fileHandler = function(req, res, next) {
 		req.files = {};
 		let busboy = new Busboy({ 
 			headers: req.headers,
-			limits: this.options.limits
+			limits: {
+				fileSize: this.options.validate.fileSize
+			}
 		});
 		storeInMemory.bind(this)(busboy, req, next);
 		req.pipe(busboy);
@@ -32,57 +114,8 @@ const fileHandler = function(req, res, next) {
 	}
 };
 
-const storeInMemory = function(busboy, req, next) {
-	busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
-		let save_filename = this.options.filename || filename;
-		let uploadInfo = {
-			options: this.options,
-			filename: save_filename,
-			mimetype: mimetype,
-			fieldname: fieldname,
-			file: file,
-			encoding: encoding
-		};
-		let file_contents = require(path.join(__dirname, "lib",this.options.method))(uploadInfo, req, next);
-		file.on("data", (data) => {
-			if (!req._file) {
-				req._file = true; // setting req._file to true to show at least 1 file has valid buffer
-			}
-			if (!req.files[fieldname]) {
-				req.files[fieldname] = {
-					_file: true // there is data in this particular file
-				}; 
-			}
-			file_contents.write(data);
-		});
-		file.on("limit", () => {
-			console.error("File limit reached");
-		});
-		file.on("end", () => {
-			if (!file.truncated && req._file) {
-				file_contents.end();
-			} else {
-				// File upload failed from limit being reached
-				req._file = undefined;
-			}
-			req._body = true;
-		});
-	});
 
-	busboy.on("field", (fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) => {
-		if (this.options.mimetype == "" || this.options.mimetype == mimetype) {
-			req.body[fieldname] = val;
-		}
-	});
-	busboy.on("finish", () => {
-		if(!req._file) {
-			// no file was uploaded
-			return next();
-		}
-	});
-};
-
-ExpressFormPost.prototype.default  = function() {
+ExpressFormPost.prototype.middleware = function() {
 	return fileHandler.bind(this);
 };
 
