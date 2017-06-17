6a1cba4113c3e79c8092b7a6f4e77faad8fcd8b0
dannycho7 <dannycho7@gmail.com>
2017-06-17 01:34:38 -0700

    config other parts for error handling

--- test/app.js
+++ test/app.js
@@ -2,15 +2,17 @@ const express = require("express");
 const path = require("path");
 const logger = require("morgan");
 require("dotenv").config({ path: path.join(__dirname, ".env") });
+
 const efp = require("../index"); // npm module
 const formPost = efp({
 	store: "disk", // Optional field; defaults to "disk-storage"
 	directory: path.join(__dirname, "tmp"),
-	validate: {
-		maxfileSize: 10000
-	},
+	maxfileSize: 10000,
 	filename: function(filename, fieldname, mimetype) {
 		return fieldname + '-' + filename;
+	},
+	validate: function(fieldname, filename, mimetype) {
+
 	}
 });
 
@@ -20,6 +22,8 @@ module.exports = (app) => {
 	app.use(express.static(path.join(__dirname, "static")));
 	app.set("view engine", "ejs");
 	app.set("views", path.join(__dirname, "views"));
-	app.use(formPost.middleware());
+	app.use(formPost.middleware(function(err) {
+		console.log(err);
+	}));
 
 };
