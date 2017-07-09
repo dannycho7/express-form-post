90b1311019de44cdc1eba309ec69449bbec6e378
dannycho7 <dannycho7@gmail.com>
2017-07-09 15:50:14 -0700

    Init skip function

--- test/filename.js
+++ test/filename.js
@@ -6,12 +6,38 @@ const FormData = require("form-data");
 const path = require("path");
 const fs = require("fs");
 
-describe("Uploading with filename that resolves to edge case", function() {
+describe("Uploading with filename with trailing slashes", function() {
 	it("Should resolve to the same file save name", function(done) {
 		createServer({
 			directory: path.join(__dirname, "tmp"),
 			filename: function(fieldname, mimetype, cb) {
 				cb("$2a$10$jGlkmUMLBdZ5LvMU0QfWpOtTiH8dqrLw6nIPaF7F_jM.tnTssT4SO/Resume_DannyHyunCho.pdf");
+			}
+		}, () => {
+			var form = new FormData();
+			let file = fs.createReadStream(path.join(__dirname, "files/medium_image.jpg"));
+			form.append("upload_img", file);
+			form.submit("http://localhost:5000", (err, res) => {
+				let data = "";
+				res.on("data", (chunk) => {
+					data += chunk;
+				});
+				res.on("end", () => {
+					let req = JSON.parse(data);
+					assert.equal(req.files.upload_img.size, 130466);
+					done();
+				});
+			});
+		});
+	});
+});
+
+describe("Uploading file with filename function throwing an error", function() {
+	it("Should throw an error", function(done) {
+		createServer({
+			directory: path.join(__dirname, "tmp"),
+			filename: function(fieldname, mimetype, cb) {
+				throw new Error("Hello!")
 			}
 		}, () => {
 			var form = new FormData();
